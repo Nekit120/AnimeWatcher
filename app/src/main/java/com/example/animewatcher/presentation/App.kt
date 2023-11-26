a42772ee36667d9efcf359b6d8a1cfcf1b877117
Nekit120 <manusa120@mail.ru>
2023-11-27 01:06:40 +0200

    Refactoring

--- app/src/main/java/com/example/animewatcher/presentation/App.kt
+++ app/src/main/java/com/example/animewatcher/presentation/App.kt
@@ -2,9 +2,6 @@ package com.example.animewatcher.presentation
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
-import com.Nekit120.codeholder.presentation.di.AppModule
-import com.Nekit120.codeholder.presentation.di.DataModule
-import com.Nekit120.codeholder.presentation.di.DomainModule
 import dagger.hilt.android.HiltAndroidApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
