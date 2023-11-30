b1c487f01fd17f99cac0f0a150bca8c2ded367ca
Nekit120 <manusa120@mail.ru>
2023-11-30 20:59:54 +0200

    Hide API

--- app/src/main/java/com/example/animewatcher/data/API/AnimeAPI.kt
+++ app/src/main/java/com/example/animewatcher/data/API/AnimeAPI.kt
@@ -1,5 +1,6 @@
 package com.example.animewatcher.data.API
 
+import com.example.animewatcher.BuildConfig
 import com.example.animewatcher.domain.model.KodikApiModel.AnimeApiList
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -7,7 +8,7 @@ import retrofit2.http.Query
 interface AnimeAPI {
     @GET("list")
     suspend fun getLastUpdatesAnime(
-        @Query("token") toket: String = "секрет",
+        @Query("token") token: String = BuildConfig.API_KEY,
         @Query("types") types:String = "anime-serial,anime",
         @Query("with_episodes") withEpisodes: Boolean = true,
         @Query("with_material_data") withMaterialData: Boolean = true,
--- app/src/main/java/com/example/animewatcher/data/repository/WatchRepositoryImpl.kt
+++ app/src/main/java/com/example/animewatcher/data/repository/WatchRepositoryImpl.kt
@@ -1,21 +1,12 @@
 package com.example.animewatcher.data.repository
 
-import android.util.Log
 import com.example.animewatcher.data.API.AnimeAPI
-import com.example.animewatcher.data.API.GetApiKeyClass
 import com.example.animewatcher.domain.model.KodikApiModel.AnimeApiItemModel
 import com.example.animewatcher.domain.repository.WatchRepository
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.io.File
-import java.io.FileInputStream
-import java.io.InputStream
-import java.util.Properties
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class WatchRepositoryImpl @Inject constructor(val retrofit: Retrofit,val getApiKeyClass: GetApiKeyClass) : WatchRepository {
+class WatchRepositoryImpl @Inject constructor(val retrofit: Retrofit) : WatchRepository {
 
     override suspend fun getLastUpdateAnime (): List<AnimeApiItemModel> {
         val animeAPI = retrofit.create(AnimeAPI::class.java)
