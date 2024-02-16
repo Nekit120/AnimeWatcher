90e691c2e675f4b099a6d0ac42b1f8c07aa63d21
Nekit120 <manusha120@mail.ru>
2024-02-16 22:12:10 +0200

    AnimeWatcher auth screen

--- app/src/main/java/com/example/animewatcher/presentation/fragments/ReleaseDateFragment.kt
+++ app/src/main/java/com/example/animewatcher/presentation/fragments/ReleaseDateFragment.kt
@@ -25,7 +25,7 @@ class ReleaseDateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_release_date, container, false)
+        return inflater.inflate(R.layout.fragment_auth, container, false)
     }
 
 
